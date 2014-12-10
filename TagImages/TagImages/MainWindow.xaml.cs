9bc2bbf5e42a687b161e026f1458a0543188ca3a
krooken <krook.jonas+github@gmail.com>
2014-12-10 21:52:57 +0100

    Searching for all image files contained in a root folder. Displaying first image.

--- TagImages/TagImages/MainWindow.xaml.cs
+++ TagImages/TagImages/MainWindow.xaml.cs
@@ -24,12 +24,18 @@ namespace TagImages
     {
         public MainWindow()
         {
+            // Folder to be scanned recursively for image files
             string scanPath = null;
 
+            // Open a folder selection dialog to choose root folder
+            // from which to search for image files.
+            // Dialog is initiated with application folder
+            // since the program is likely to be at catalog root.
             FolderBrowserDialog dialog = new FolderBrowserDialog();
             dialog.SelectedPath = Directory.GetCurrentDirectory();
             DialogResult result = dialog.ShowDialog();
             
+            // Exit if cancelled.
             if (result == System.Windows.Forms.DialogResult.OK)
             {
                 scanPath = dialog.SelectedPath;
@@ -39,7 +45,48 @@ namespace TagImages
                 Environment.Exit(1);
             }
 
+            // Bring up GUI
             InitializeComponent();
+
+            // What to search for.
+            // Mask is catalog root folder plus search expression.
+            string mask = scanPath + "\\*.*";
+            // Acquire list of all image files.
+            System.Collections.Generic.List<string> list = GetAllFiles(mask, (info) => IsImageFile(info)).ToList();
+
+            // Show first image in the list of files.
+            BitmapImage img = new BitmapImage(new Uri(list[0]));
+            this.PictureFrame.Source = img;
+            
+        }
+
+        // List all files recurively from a root folder defined by path.
+        // Check file lambda expression that determines which files to return.
+        public static IEnumerable<string> GetAllFiles(string path, Func<FileInfo, bool> checkFile = null)
+        {
+            // Get mask, i.e., file part of path. ("*.*" if path is "C:\\yadayadayada\\*.*")
+            string mask = System.IO.Path.GetFileName(path);
+            if (string.IsNullOrEmpty(mask))
+                mask = "*.*";
+            // Get base path of folder, i.e., C:\\yadayadayada\\
+            path = System.IO.Path.GetDirectoryName(path);
+            // List all files
+            string[] files = Directory.GetFiles(path, mask, SearchOption.AllDirectories);
+            // Go though all files and determine whther they are to be included in the list.
+            foreach (string file in files)
+            {
+                if (checkFile == null || checkFile(new FileInfo(file)))
+                    yield return file;
+            }
+        }
+
+        // Checks whether a file is an jpeg or tiff file.
+        public static bool IsImageFile(FileInfo info)
+        {
+            string extension = System.IO.Path.GetExtension(info.Name).ToLower();
+            bool isJpeg = extension == ".jpeg" || extension == ".jpg";
+            bool isTiff = extension == ".tiff" || extension == ".tif";
+            return isJpeg || isTiff;
         }
     }
 }
